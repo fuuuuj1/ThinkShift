baf9efcdd04ef977a0cf9ecc9dd1d1713db890bd
fuuuu <sanchan.fuu@gmail.com>
2024-07-21 11:13:26 +0900

    feat: jsonからランダムに文字列を取得して描画する機能を実装

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,27 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, loaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import prompts from "../../public/prompts.json";
 
+// import した prompts を loaderFunction で返す
+export const loader: loaderFunction = async () => {
+  return json({ prompt: prompts[Math.floor(Math.random() * prompts.length)] });
+};
+
+// メタ情報を設定する関数
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Think Shift" },
+    { name: "description", content: "May this help you shift your thinking in difficult situations." },
   ];
 };
 
+// Indexコンポーネントを定義してloaderからデータを取得し表示します
 export default function Index() {
+  const { prompt } = useLoaderData();
   return (
     <div className="font-sans p-4">
-      ここにコンテンツを追加してください
+      <p>{prompt}</p>
     </div>
   );
 }
